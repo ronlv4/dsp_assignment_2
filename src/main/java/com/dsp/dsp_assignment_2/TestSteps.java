368af7db44021d441d5d8857cef4aac028b756c3
Ron Levi <ron7@post.bgu.ac.il>
2022-06-01 10:38:58 +0300

    no step1

--- src/main/java/com/dsp/dsp_assignment_2/TestSteps.java
+++ src/main/java/com/dsp/dsp_assignment_2/TestSteps.java
@@ -10,7 +10,7 @@ public class TestSteps {
 
     public static void main(String[] args) throws Exception {
         String[] pipe = new String[2];
-        step1BigramDecadeCount.main(pipe);
+//        step1BigramDecadeCount.main(pipe);
         logger.info("about to start step 2");
         step2SortBigramsDecadeByOccurrence.main(new String[]{"/home/hadoop/output1654066896913"});
 //        step2SortBigramsDecadeByOccurrence.main(pipe);
