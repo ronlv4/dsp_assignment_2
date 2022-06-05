f0c4e3f9d231052997f4412a915aa4140589f43d
Ron Levi <ron7@post.bgu.ac.il>
2022-06-05 17:08:20 +0300

    skip words on step 2

--- src/main/java/com/dsp/mr_app/step2BigramDecadeCount.java
+++ src/main/java/com/dsp/mr_app/step2BigramDecadeCount.java
@@ -10,11 +10,18 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.StringUtils;
 import org.apache.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class step2BigramDecadeCount {
     /*
@@ -40,6 +47,39 @@ public class step2BigramDecadeCount {
             Key: <w1 w2:decade>
             Value: occurrences of the bigram <w1 w2> in the year
          */
+
+        private boolean caseSensitive;
+        private Set<String> patternsToSkip = new HashSet<String>();
+
+        private Configuration conf;
+        private BufferedReader fis;
+
+        @Override
+        public void setup(Context context) throws IOException, InterruptedException {
+            conf = context.getConfiguration();
+            caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
+            if (conf.getBoolean("wordcount.skip.patterns", false)) {
+                URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
+                for (URI patternsURI : patternsURIs) {
+                    Path patternsPath = new Path(patternsURI.getPath());
+                    String patternsFileName = patternsPath.getName().toString();
+                    parseSkipFile(patternsFileName);
+                }
+            }
+        }
+
+        private void parseSkipFile(String fileName) {
+            try {
+                fis = new BufferedReader(new FileReader(fileName));
+                String pattern = null;
+                while ((pattern = fis.readLine()) != null) {
+                    patternsToSkip.add(pattern);
+                }
+            } catch (IOException ioe) {
+                System.err.println("Caught exception while parsing the cached file '"
+                        + StringUtils.stringifyException(ioe));
+            }
+        }
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             logger.info("got from record reader the line " + value);
@@ -89,7 +129,7 @@ public class step2BigramDecadeCount {
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException, URISyntaxException {
         if (args.length < 1) {
             logger.error("not place to store output path");
             System.exit(1);
@@ -103,6 +143,12 @@ public class step2BigramDecadeCount {
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(BigramDecade.class);
         job.setOutputValueClass(IntWritable.class);
+        job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
+//        job.addCacheFile(new Path("/home/hadoop/stop-words/eng-stopwords.txt").toUri());
+//        job.addCacheFile(new Path("/home/hadoop/stop-words/heb-stopwords.txt").toUri());
+        job.addCacheFile(new URI(BUCKET_HOME_SCHEME + "stop-words/eng-stopwords.txt"));
+        job.addCacheFile(new URI(BUCKET_HOME_SCHEME + "stop-words/heb-stopwords.txt"));
+
         //FileInputFormat.addInputPath(job, new Path("s3://datasets.elasticmapreduce/ngrams/books/20090715/eng-gb-all/2gram/data"));
         FileInputFormat.addInputPath(job, new Path(BUCKET_HOME_SCHEME + "google-2grams/"));
 //        FileInputFormat.addInputPath(job, new Path("/home/hadoop/input/"));
