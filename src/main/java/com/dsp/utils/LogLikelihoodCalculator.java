2a5e13361148ba681f6fa5e337c86d78cabc19c9
Ron Levi <ron7@post.bgu.ac.il>
2022-06-05 17:38:57 +0300

    doc to log calc

--- src/main/java/com/dsp/utils/LogLikelihoodCalculator.java
+++ src/main/java/com/dsp/utils/LogLikelihoodCalculator.java
@@ -2,10 +2,10 @@ package com.dsp.utils;
 
 public class LogLikelihoodCalculator {
 
-    private final Integer c1;
-    private final Integer c2;
-    private final Integer c12;
-    private final Integer N;
+    private final Integer c1; // number of occurrences of w1 in the corpus
+    private final Integer c2; // number of occurrences of w2 in the corpus
+    private final Integer c12; // number of occurrences of w1 w2 in the corpus
+    private final Integer N; // total number of words in the corpus
 
     public LogLikelihoodCalculator(Integer c1, Integer c2, Integer c12, Integer n) {
         this.c1 = c1;
