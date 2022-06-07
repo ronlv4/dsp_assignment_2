cecb06ef64a1efa8c1ba4ec53431c08dc8e1b25b
Ron Levi <ron7@post.bgu.ac.il>
2022-06-07 11:56:30 +0300

    log errors

--- src/main/java/com/dsp/mr_app/step1UnigramCount.java
+++ src/main/java/com/dsp/mr_app/step1UnigramCount.java
@@ -75,6 +75,8 @@ public class step1UnigramCount {
         private boolean filterByLanguage(URI uri, Configuration conf){
             String language = conf.get("wordcount.input.language");
             String fileName = FilenameUtils.getName(uri.getPath());
+            logger.debug("uri path: " + uri.getPath());
+            logger.debug("filename: " + fileName);
             return fileName.startsWith(language);
         }
 
