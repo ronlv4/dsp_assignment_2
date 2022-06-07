a5c491d0aa564cf03bbdc845633ab0a2c098545c
Ron Levi <ron7@post.bgu.ac.il>
2022-06-07 13:05:00 +0300

    args on main

--- src/main/java/com/dsp/dsp_assignment_2/TestSteps.java
+++ src/main/java/com/dsp/dsp_assignment_2/TestSteps.java
@@ -13,25 +13,21 @@ public class TestSteps {
 
     public static void main(String[] args) throws Exception {
 
-        logger.debug("args" + Arrays.toString(args));
+        logger.debug("args: " + Arrays.toString(args));
 
-        if (args.length > 2 || args.length < 1) { //TODO: when running in intelliJ args[0] is not path to program
+        if (args.length > 3 || args.length < 2) {
             String usage = "Usage: <language> [case_sensitive=false]";
             System.out.println(usage);
             System.exit(1);
         }
-        args[1] = StringUtils.isEmpty(args[1]) ? "false" : "true";
-        String[] stepsArgs = new String[args.length + 1];
-        System.arraycopy(args, 0, stepsArgs, 0, args.length);
-        logger.debug("stepsArgs" + Arrays.toString(stepsArgs));
+        args[2] = StringUtils.isEmpty(args[1]) ? "false" : "true";
 
         try {
-            String[] pipe = new String[3];
-            step1UnigramCount.main(pipe);
-            step2BigramDecadeCount.main(pipe);
-            step3MergeUnigramsBigramsLeft.main(pipe);
-            step4MergeUnigramsBigramsRight.main(pipe);
-            step5CalculateLogLikelihood.main(pipe);
+            step1UnigramCount.main(args);
+            step2BigramDecadeCount.main(args);
+            step3MergeUnigramsBigramsLeft.main(args);
+            step4MergeUnigramsBigramsRight.main(args);
+            step5CalculateLogLikelihood.main(args);
         } catch (Exception e){
             logger.error(e.getMessage(), e);
             throw e;
